addddeceaf757fc9da4a5285bd5fff5c989396ec
杨帆 <hzyang_fan@corp.netease.com>
2015-03-25 16:18:45 +0800

    doFormatURI

--- javascript/define.js
+++ javascript/define.js
@@ -1,6 +1,6 @@
 (function (_d, _g) {
 
-    var __config = {root: {/*lib,pro,platform*/lib: 'javascript/lib'}/*native,charset,global,platform*/};
+    var __config = {site: {/*lib,pro,platform*/lib: 'javascript/lib/'}/*native,charset,global,platform*/};
     /*
      * 判断是否字符串
      * @param  {Varable} _data 数据
@@ -16,62 +16,60 @@
      * @return {String}      格式化后地址
      */
     var _doFormatURI = (function () {
-        var _xxx = !1,
-            _reg = /{(.*?)}/gi,
+        var _init = !1,
             _reg1 = /([^:])\/+/g,
-            _reg3 = /[^\/]*$/,
             _reg4 = /\.js$/i,
-            _reg5 = /^[{\/\.]/,
-            _anchor = _d.createElement('a');
+            _reg5 = /^[\/\.]/,
+            _anchor = _d.createElement('a'),
+            _classMap = {
+                text: function (_uri) {
+                    // todo
+                },
+                json: function (_uri) {
+                    // todo
+                }
+            },
+            _browerMap = ['webkit', 'ie', 'firefox'];
         var _absolute = function (_uri) {
             return _uri.indexOf('://') > 0;
         };
-        var _slash = function (_uri) {
-            return _uri.replace(_reg1, '$1/');
-        };
         var _append = function () {
-            if (_xxx) return;
-            _xxx = !0;
+            if (_init) return;
+            _init = !0;
             _anchor.style.display = 'none';
             document.body.appendChild(_anchor);
         };
-        var _root = function (_uri) {
-            return _uri.replace(_reg3, '');
+        var _parse = function (_uri) {
+            var _brr = [],
+                _type = null,
+                _arr = _uri.split('!'),
+                _fun = _classMap[_arr[0].toLowerCase()];
+            if (_arr.length > 1) {
+                _type = _arr.shift();
+            }
+            _brr.push(_arr.join('!'));
+            _brr.push(_fun || 'todo');
+            _brr.push(_type);
+            return _brr;
         };
         var _format = function (_uri) {
             _append();
             _anchor.href = _uri;
             _uri = _anchor.href;
-            return _absolute(_uri) && _uri.indexOf('./') < 0 ?
-                _uri : _anchor.getAttribute('href', 4); // ie6/7
+            return _absolute(_uri) && _uri.indexOf('./') < 0 ? _uri : _anchor.getAttribute('href', 4); // ie6/7
         };
-        var _amdpath = function (_uri, _type) {
-            // start with {xx} or /xx/xx or ./ or ../
-            // end with .js
-            // absolute uri
-            if (_reg4.test(_uri) ||
-                _reg5.test(_uri) ||
-                _absolute(_uri)) {
-                return _uri;
-            }
-            // lib/base/klass -> {lib}base/klass.js
-            // pro/util/a     -> {pro}util/a.js
-            var _arr = _uri.split('/'),
-                _path = __config.root[_arr[0]],
-                _sufx = !_type ? '.js' : _type;
-            if (!!_path) {
-                _arr.shift();
-                return _path + _arr.join('/') + _sufx;
-            }
-            // for base/klass -> {lib}base/klass.js
-            return '{lib}' + _arr.join('/') + _sufx;
+        var _amdPath = function (_uri) {
+            var _arr = _parse(_uri),
+                _site = __config.site[_arr[2]] || '',
+                _sufx = _reg4.test(_uri) ? '' : '.js';
+            return (_site + _arr[0] + _sufx).replace(_reg1, '$1/');
         };
-        return function (_uri, _base, _type) {
+        return function (_uri) {
             if (_isTypeOf(_uri, 'Array')) {
                 var _list = [];
                 for (var i = 0; i < _uri.length; i++) {
                     _list.push(
-                        _doFormatURI(_uri[i], _base, _type)
+                        _doFormatURI(_uri[i])
                     );
                 }
                 return _list;
@@ -80,19 +78,10 @@
             if (_absolute(_uri)) {
                 return _format(_uri);
             }
-            if (_base && _uri.indexOf('.') == 0) {
-                _uri = _root(_base) + _uri;
-            }
-            _uri = _slash(_amdpath(_uri, _type));
-            var _uri = _uri.replace(
-                _reg, function ($1, $2) {
-                    return __config.root[$2] || $2;
-                }
-            );
-            return _format(_uri);
+            return _format(_amdPath(_uri));
         };
     })();
 
-    console.log(_doFormatURI(['{lib}/framework.js'], true))
+    console.log(_doFormatURI('lib!./framework'))
 
 })(document, window);
